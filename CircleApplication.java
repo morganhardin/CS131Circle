7c64e0cc7b9fb5570bb0883c4889c03c73d2e70a
morganhardin <mhardin5@bellarmine.edu>
2021-01-21 15:19:18 -0500

    first commit

---
+++ CircleApplication.java
@@ -0,0 +1,12 @@
+public class CircleApplication 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Circle aCircle = new Circle();
+		double area = aCircle.calculateArea(100.3);
+		System.out.println(area);
+	}
+
+}
